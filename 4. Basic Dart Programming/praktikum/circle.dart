5f23f1791008031c9a6a9588bd4be2d40e5444f3
faisalapt <faisal.aprianto@student.umn.ac.id>
2022-08-29 14:03:50 +0700

    section 6

---
+++ 4. Basic Dart Programming/praktikum/circle.dart
@@ -0,0 +1,6 @@
+void main(List<String> args) {
+  var r = 7; //Jari-jari lingakaran
+  var phi = 3.14; //nilai phi
+  var luas = phi * r * r; //menghitung luas lingakaran
+  print(luas); //menampilkan luas lingkaran
+}
