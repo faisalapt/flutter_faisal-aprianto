d9980c24767e69c3de500d6059ac7dbd30f2bc08
Faisal Aprianto <faisal.aprianto@student.umn.ac.id>
2022-09-20 11:39:39 +0700

    section15

---
+++ 13. Flutter Layout/praktikum/tugas1/lib/data.dart
@@ -0,0 +1,9 @@
+var data = [
+  {"name": "Lanne Graham", "number": "1-770-736-8031 x56442"},
+  {"name": "Ervin Howell", "number": "010-692-6593 x09125"},
+  {"name": "Clementine Bauch", "number": "1-463-123-4447"},
+  {"name": "Patricia Lebsack", "number": "493-170-9623 x156"},
+  {"name": "Chelsey Dietrich", "number": "(254)954-1289"},
+  {"name": "Mrs. Dennis Schulist", "number": "1-477-935-8478 x6430"},
+  {"name": "Kurtis Weissnat", "number": "210.067.6132"}
+];
